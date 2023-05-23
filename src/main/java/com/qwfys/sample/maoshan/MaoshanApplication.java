4015def26da9421539f90e9f92e9f0646c1d050f
liuwenke <qwfys200@qq.com>
2023-05-23 23:35:44 +0800

    add project structure

---
+++ src/main/java/com/qwfys/sample/maoshan/MaoshanApplication.java
@@ -0,0 +1,13 @@
+package com.qwfys.sample.maoshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaoshanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaoshanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qwfys/sample/maoshan/MaoshanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwfys.sample.maoshan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaoshanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
