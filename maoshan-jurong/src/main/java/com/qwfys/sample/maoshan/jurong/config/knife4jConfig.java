e0307d86f087ee094a0131e5653a8ee52fbe2351
liuwenke <qwfys200@qq.com>
2023-05-24 17:10:18 +0800

    update

---
+++ maoshan-jurong/src/main/java/com/qwfys/sample/maoshan/jurong/config/knife4jConfig.java
@@ -0,0 +1,40 @@
+package com.qwfys.sample.maoshan.jurong.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liuwenke
+ * @since 0.0.1
+ */
+@Configuration
+public class knife4jConfig {
+
+    @Bean
+    public GroupedOpenApi baseRestApi() {
+        return GroupedOpenApi.builder()
+                .group("接口文档")
+                .packagesToScan("com.qwfys.sample.maoshan")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenApi() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Service Consumer接口文档")
+                                .description("Service Consumer接口文档")
+                                .version("0.0.1-SNAPSHOT")
+                                .license(
+                                        new License()
+                                                .name("使用请遵守MIT License授权协议")
+                                                .url("https://github.com/ab-sample/maoshan")
+                                )
+                );
+    }
+}
